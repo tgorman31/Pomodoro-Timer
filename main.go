476bc19aa16ebdc1168d3f1c6411ad8e4e9cf40f
tgorman31 <gorman.thomas31@gmail.com>
2024-05-13 21:43:47 +1200

    Adding bubbletea to project

--- main.go
+++ main.go
@@ -1,47 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-	"time"
+	tm "pomodoro-timer/time"
 
 	in "github.com/tgorman31/Go-Modules/input"
 )
 
-func Hello() string {
-	return "Hello World"
-}
-
-// Takes an input of a timer name and minutes and seconds and starts a timer
-func Timer(name string, min int, sec int) {
-	s := (min * 60) + sec
-	timer := time.NewTimer(time.Duration(s) * time.Second)
-
-	fmt.Printf("Running %s timer for %v seconds \n", name, s)
-	<-timer.C
-	fmt.Printf("%s timer completed \n", name)
-}
-
-func Time_Parse(time string) (min int, sec int) {
-
-	t := strings.Split(time, ":")
-
-	m, _ := strconv.Atoi(t[0])
-	s, _ := strconv.Atoi(t[1])
-
-	return m, s
-}
-
 func main() {
-	fmt.Println(Hello())
 
-	wrk := in.User_Input("Please enter a work time as such ##:##")
-	brk := in.User_Input("Please enter a break time as such ##:##")
+	wrk := in.User_Input("Please enter a work tm as such ##:##")
+	brk := in.User_Input("Please enter a break tm as such ##:##")
 
-	wMin, wSec := Time_Parse(wrk)
-	bMin, bSec := Time_Parse(brk)
+	wMin, wSec := tm.Time_Parse(wrk)
+	bMin, bSec := tm.Time_Parse(brk)
 
-	Timer("Work", wMin, wSec)
-	Timer("Break", bMin, bSec)
+	tm.Timer("Work", wMin, wSec)
+	tm.Timer("Break", bMin, bSec)
+	tm.Timer_Tea("NewWork", bMin, bSec)
 }
